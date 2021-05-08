ae62390ab2a9dbd4b7b38c4d9aeeb98d0b05c34a
Gabriel <gabriel.nascimento@dcomp.ufs.br>
2021-05-08 10:22:02 -0300

    estilizando tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
 import { HiOutlineMail } from "react-icons/hi";
 import { RiLock2Fill } from "react-icons/ri";
+import { HiEye, HiEyeOff } from "react-icons/hi";
 // eslint-disable-next-line no-unused-vars
 function Login () {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShow(!show); 
+  }
+
+
   return (
     <div className="login">
       <div className="login-logo">
@@ -14,14 +25,36 @@ function Login () {
         <h1>Acessar App</h1>
         <div className="loginInputEmail">
           <HiOutlineMail />
-          <input type="text" placeholder="Digite um Email" />
+          <input 
+          type="text" 
+          placeholder="Digite um Email" 
+          value={email} 
+          onChange={e => setEmail(e.target.value)} />
         </div>
 
         <div className="loginInputPassword">
           <RiLock2Fill />
-          <input type="text" placeholder="Digite sua senha" />
+          <input 
+          type={show ? "text" : "password"}
+          placeholder="Digite sua senha" 
+          value={password} 
+          onChange={e => setPassword(e.target.value)} 
+          />
+          <div className="login-eye">
+            {show ? (
+              <HiEye 
+                size={20}
+                onClick={handleClick}
+              />
+            ) : (
+              <HiEyeOff 
+              size={20}
+              onClick={handleClick}
+              />
+            )}
+          </div>
         </div>
-        <button type="submit">
+        <button  type="submit">
           Entrar
         </button>
         <h4>Não tenho conta</h4>
